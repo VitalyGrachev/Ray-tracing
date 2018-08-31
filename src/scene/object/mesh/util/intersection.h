05f12638c0a28f11551906bd23d11dbf50651d5a
Vitaly Grachev <VitalyGrachev@users.noreply.github.com>
2018-08-31 19:33:48 +0700

    Create intersection class. Find possible intersection of ray and triangle

---
+++ src/scene/object/mesh/util/intersection.h
@@ -0,0 +1,32 @@
+#ifndef RAY_TRACING_INTERSECTION_H
+#define RAY_TRACING_INTERSECTION_H
+
+#include "../primitives/triangle.h"
+#include "../../../../util/vector.h"
+#include "../../../camera/ray.h"
+
+class Intersection {
+public:
+    static std::optional<Intersection> find(const Triangle & triangle, const Ray & ray);
+
+    Intersection(const Triangle & triangle, const Vec3 & point, const Vec3 & normal);
+
+    Intersection(const Intersection &) = default;
+
+    ~Intersection() = default;
+
+    Intersection & operator=(const Intersection &) = default;
+
+    const Vec3 & point() const { return point_; }
+
+    const Vec3 & normal() const { return normal_; }
+
+    const Triangle & triangle() const { return *triangle_; }
+
+private:
+    const Triangle * triangle_;
+    Vec3 point_;
+    Vec3 normal_;
+};
+
+#endif //RAY_TRACING_INTERSECTION_H

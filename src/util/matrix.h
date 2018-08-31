df8f9acdb1c187f484e492fe7c75b3b4e9b52f2c
Vitaly Grachev <VitalyGrachev@users.noreply.github.com>
2018-08-31 19:22:45 +0700

    Add linear algebra utilities

---
+++ src/util/matrix.h
@@ -0,0 +1,10 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <QMatrix3x3>
+#include <QMatrix4x4>
+
+using Mat3 = QMatrix3x3;
+using Mat4 = QMatrix4x4;
+
+#endif //MATRIX_H
---
+++ src/util/vector.h
@@ -0,0 +1,12 @@
+#ifndef RAY_TRACING_VECTOR_H
+#define RAY_TRACING_VECTOR_H
+
+#include <QVector2D>
+#include <QVector3D>
+#include <QVector4D>
+
+using Vec2 = QVector2D;
+using Vec3 = QVector3D;
+using Vec4 = QVector4D;
+
+#endif //RAY_TRACING_VECTOR_H

3ceedbae93e9e5be7d833b890a311089a2766210
Vitaly Grachev <VitalyGrachev@users.noreply.github.com>
2018-09-04 14:07:44 +0700

    Create Transform class

---
+++ src/util/transform.h
@@ -0,0 +1,11 @@
+#ifndef RAY_TRACING_TRANSFORM_H
+#define RAY_TRACING_TRANSFORM_H
+
+/**
+ * Describes elements position, rotation and scale in world coordinate system
+ */
+class Transform {
+
+};
+
+#endif //RAY_TRACING_TRANSFORM_H

e18bed121e26e9d763691db6d50458151996e452
Vitaly Grachev <VitalyGrachev@users.noreply.github.com>
2018-09-04 13:52:06 +0700

    Store in Object shared Mesh and Material
    Saves memory in case of bunch of Objects at the Scene have same Meshes and/or Materials.

--- src/scene/object/object.h
+++ src/scene/object/object.h
@@ -1,12 +1,19 @@
 #ifndef RAY_TRACING_OBJECT_H
 #define RAY_TRACING_OBJECT_H
 
+#include <memory>
 #include "mesh/mesh.h"
+#include "material/material.h"
 
 class Object {
 public:
+    const Mesh & mesh() const { return *mesh_; }
+
+    const Material & material() const { return *material_; }
+
 private:
-    Mesh mesh_;
+    std::shared_ptr<Mesh> mesh_;
+    std::shared_ptr<Material> material_;
 };
 
 

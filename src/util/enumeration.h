06a25791d25b2a2bb6235ef650043074e79efb53
Vitaly Grachev <VitalyGrachev@users.noreply.github.com>
2018-09-04 13:34:15 +0700

    Add begin() and end() methods to Enumeration
    Simplifies usage allowing to use range-base for.

--- src/util/enumeration.h
+++ src/util/enumeration.h
@@ -1,12 +1,23 @@
 #ifndef RAY_TRACING_ENUMERATION_H
 #define RAY_TRACING_ENUMERATION_H
 
-template <class Iterator>
-struct Enumeration {
-    using iterator_type = Iterator;
+#include <iterator>
 
-    Iterator first;
-    Iterator last;
+template<class Iterator>
+class Enumeration {
+public:
+    using value_type = typename std::iterator_traits<Iterator>::value_type;
+    using iterator = Iterator;
+
+    Enumeration(Iterator first, Iterator last) : first_(first), last_(last) {}
+
+    iterator begin() const { return first_; }
+
+    iterator end() const { return last_; }
+
+private:
+    Iterator first_;
+    Iterator last_;
 };
 
 #endif //RAY_TRACING_ENUMERATION_H

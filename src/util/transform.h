1c2e1caf65d2efe6ee2fef04a4d6b7f2cc2782a2
Vitaly Grachev <VitalyGrachev@users.noreply.github.com>
2018-09-04 14:23:04 +0700

    Define Transform's interface

--- src/util/transform.h
+++ src/util/transform.h
@@ -1,11 +1,30 @@
 #ifndef RAY_TRACING_TRANSFORM_H
 #define RAY_TRACING_TRANSFORM_H
 
+#include "vector.h"
+
 /**
  * Describes elements position, rotation and scale in world coordinate system
  */
 class Transform {
+public:
+    Transform();
 
+    Transform(const Transform & other);
+
+    ~Transform();
+
+    Transform & operator=(const Transform & other);
+
+    Transform & translate(const Vec3 & vector);
+
+    Transform & rotate(float angles, const Vec3 & axis);
+
+    Transform & scale(const Vec3 & factors);
+
+    Vec3 map_to_world(const Vec3 & local) const;
+
+    Vec3 map_to_local(const Vec3 & world) const;
 };
 
 #endif //RAY_TRACING_TRANSFORM_H

dc8b98c213283b3898aca92b1071744f14877f22
Vitaly Grachev <VitalyGrachev@users.noreply.github.com>
2018-09-05 11:27:11 +0700

    Create Geometry interface and define it's contract

---
+++ src/geometry/geometry.h
@@ -0,0 +1,22 @@
+#ifndef RAY_TRACING_GEOMETRY_H
+#define RAY_TRACING_GEOMETRY_H
+
+#include <optional>
+#include "intersection.h"
+
+/**
+ * Interface. Represents piece of geometry, which ray can intersect with
+ */
+class Geometry {
+public:
+    virtual ~Geometry() = default;
+
+    /**
+     * Finds closest to ray origin point, where ray intersects with this geometry
+     * @param ray
+     * @return std::optional containing found intersection or empty std::optional if there is no intersection
+     */
+    virtual std::optional<Intersection> find_intersection(const Ray & ray) const = 0;
+};
+
+#endif //RAY_TRACING_GEOMETRY_H

287a0d8560c27ba9c03d3c3be68d046e0f5626e7
Vitaly Grachev <VitalyGrachev@users.noreply.github.com>
2018-09-05 21:59:31 +0700

    Define contract of Material interface

--- src/scene/object/material/material.h
+++ src/scene/object/material/material.h
@@ -1,10 +1,26 @@
 #ifndef RAY_TRACING_MATERIAL_H
 #define RAY_TRACING_MATERIAL_H
 
+#include "../../../util/color.h"
 
+/**
+ * Interface. Represents optical properties of object
+ */
 class Material {
+public:
+    virtual ~Material() = default;
 
+    virtual Color color_at(const Vec2 & tex_coords) const = 0;
+
+    virtual Vec3 diffuse_reflectance_at(const Vec2 & tex_coords) const = 0;
+
+    virtual Vec3 specular_reflectance_at(const Vec2 & tex_coords) const = 0;
+
+    virtual Vec3 specular_power_at(const Vec2 & tex_coords) const = 0;
+
+    virtual float refractive_index_at(const Vec2 & tex_coords) const = 0;
+
+    virtual float shininess_at(const Vec2 & tex_coords) const = 0;
 };
-
 
 #endif //RAY_TRACING_MATERIAL_H

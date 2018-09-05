6f0024346c9a96ef35058948b52d86b7420ffb03
Vitaly Grachev <VitalyGrachev@users.noreply.github.com>
2018-09-05 15:34:15 +0700

    Use Geometry insted of Mesh to represend LightSource's visible geometry

--- src/scene/light/light_source.h
+++ src/scene/light/light_source.h
@@ -2,15 +2,22 @@
 #define RAY_TRACING_LIGHT_SOURCE_H
 
 #include "light.h"
-#include "../object/mesh/mesh.h"
+#include "../../geometry/geometry.h"
 
 /**
  * Piece of geometry that emit light
  */
 class LightSource {
 public:
+    /**
+     * @return Light emitted by light source
+     */
     const Light & light() const;
-    const Mesh & mesh() const;
+
+    /**
+     * @return visible geometry of light source
+     */
+    const Geometry & geometry() const;
 };
 
 #endif //RAY_TRACING_LIGHT_SOURCE_H

7098a0afa5bac125a4b0c9e698e29b08fbcd6415
Vitaly Grachev <VitalyGrachev@users.noreply.github.com>
2018-09-06 15:13:06 +0700

    Implement Light class

--- src/scene/light/light.cpp
+++ src/scene/light/light.cpp
@@ -1,1 +1,7 @@
 #include "light.h"
+
+#include <algorithm>
+
+Light::Light(const Color & color, float intensity)
+        : color_(color),
+          intensity_(std::clamp(intensity, 0.0f, 1.0f)) {}
--- src/scene/light/light.h
+++ src/scene/light/light.h
@@ -1,12 +1,41 @@
 #ifndef RAY_TRACING_LIGHT_H
 #define RAY_TRACING_LIGHT_H
 
-#include "../../util/vector.h"
+#include "../../util/color.h"
 
-class Light {
+/**
+ * Represents properties of emitted light
+ */
+class Light final {
 public:
-    const Vec3 & color() const;
-    float intensity() const;
+    /**
+     * Creates light with given color and intensity
+     * @param color light's color
+     * @param intensity light's intensity, clamped between 0 and 1
+     */
+    Light(const Color & color, float intensity);
+
+    Light(const Light & other) = default;
+
+    ~Light() = default;
+
+    Light & operator=(const Light & other) = default;
+
+    /**
+     * Returns light color
+     * @return light color
+     */
+    const Color & color() const { return color_; }
+
+    /**
+     * Returns light intensity in interval [0, 1].
+     * @return light intensity
+     */
+    float intensity() const { return intensity_; }
+
+private:
+    Color color_;
+    float intensity_;
 };
 
 #endif //RAY_TRACING_LIGHT_H

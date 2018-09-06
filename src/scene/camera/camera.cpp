1557e4e2f8d75300279f3a48c8946b2278a9b17b
Vitaly Grachev <VitalyGrachev@users.noreply.github.com>
2018-09-06 12:38:07 +0700

    Define Camera's contract

--- src/scene/camera/camera.cpp
+++ src/scene/camera/camera.cpp
@@ -1,1 +1,8 @@
 #include "camera.h"
+
+Camera::Camera()
+        : fov_(Vec2(1, 1)) {}
+
+void Camera::set_field_of_view(const Vec2 & fov) {
+    fov_ = fov;
+}
--- src/scene/camera/camera.h
+++ src/scene/camera/camera.h
@@ -3,12 +3,46 @@
 
 #include "../../util/transform.h"
 
-class Camera {
+class Camera final {
 public:
+    /**
+     * Creates camera with default transform and field of view.
+     * Default transform sets camera into the origin of world coordinate system with camera looking along X axis.
+     * Default field of view is 1 radian for horizontal and vertical fields of view.
+     */
+    Camera();
+
+    Camera(const Camera & other) = default;
+
+    ~Camera() = default;
+
+    Camera & operator=(const Camera & other) = default;
+
+    /**
+     * Returns transform, which represent placement of camera in world coordinate system
+     * @return transform of the camera
+     */
+    Transform & transform() { return transform_; }
+
     const Transform & transform() const { return transform_; }
+
+    /**
+     * Returns Vec2 of angles, which define camera's field of view
+     * First component contains angle in radians, which define horizontal field of view, in interval (-pi, pi)
+     * Second component contains angle in radians, which define vertical field of view, in interval (-pi, pi)
+     * @return horizontal and vertical angles, which define camera's field of view
+     */
+    const Vec2 & field_of_view() const { return fov_; }
+
+    /**
+     * Sets horizontal and vertical angles, which define camera's field of view
+     * @param fov Vec2, whose first and second components contain horizontal and vertical angles, which define camera's field of view, expressed in radians respectively
+     */
+    void set_field_of_view(const Vec2 & fov);
 
 private:
     Transform transform_;
+    Vec2 fov_;
 };
 
 #endif //RAY_TRACING_CAMERA_H

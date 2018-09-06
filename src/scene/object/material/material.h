1d0adebd29208c195cbbe706740faa3f9e0b31cc
Vitaly Grachev <VitalyGrachev@users.noreply.github.com>
2018-09-06 10:39:14 +0700

    Change Material's contract

--- src/scene/object/material/material.h
+++ src/scene/object/material/material.h
@@ -16,11 +16,13 @@ public:
 
     virtual Vec3 specular_reflectance_at(const Vec2 & tex_coords) const = 0;
 
-    virtual Vec3 specular_power_at(const Vec2 & tex_coords) const = 0;
-
-    virtual float refractive_index_at(const Vec2 & tex_coords) const = 0;
-
     virtual float shininess_at(const Vec2 & tex_coords) const = 0;
+
+    virtual bool has_refraction_at(const Vec2 & tex_coords) const = 0;
+
+    virtual Vec3 refracted_amount_at(const Vec2 & tex_coords) const = 0;
+
+    virtual Vec3 refractive_index_at(const Vec2 & tex_coords) const = 0;
 };
 
 #endif //RAY_TRACING_MATERIAL_H

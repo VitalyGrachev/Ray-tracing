3e2c76d9bd93548bb9317b70e422a163dc3da1ea
Vitaly Grachev <VitalyGrachev@users.noreply.github.com>
2018-09-08 10:33:31 +0700

    Rename some methods. Add doc comments

--- src/util/transform.h
+++ src/util/transform.h
@@ -8,6 +8,14 @@
  */
 class Transform {
 public:
+    /**
+     * @return identity transform, which does nothing
+     */
+    static Transform indentity();
+
+    /**
+     * Creates identity transform, which does nothing
+     */
     Transform();
 
     Transform(const Transform & other);
@@ -16,15 +24,69 @@ public:
 
     Transform & operator=(const Transform & other);
 
+    /**
+     * Adds to the transform translation by the components of vector
+     * @param vector vector to translate by
+     * @return modified transform
+     */
     Transform & translate(const Vec3 & vector);
 
-    Transform & rotate(float angles, const Vec3 & axis);
+    /**
+     * Adds to the transform rotation through angle radians about vector axis
+     * @param angle angle to rotate through
+     * @param axis axis to rotate about
+     * @return modified transform
+     */
+    Transform & rotate(float angle, const Vec3 & axis);
 
+    /**
+     * Adds to transform scale by the components of factor
+     * @param factors
+     * @return modified transform
+     */
     Transform & scale(const Vec3 & factors);
 
-    Vec3 map_to_world(const Vec3 & local) const;
+    /**
+     * Adds to transform scale by the given factor
+     * @param factor factor to scale by
+     * @return modified transform
+     */
+    Transform & scale(float factor);
 
-    Vec3 map_to_local(const Vec3 & world) const;
+    /**
+     * Returns forward-facing vector.
+     * Same as map_to_world(Vec3(1, 0, 0)
+     * @return forward-facing vector
+     */
+    Vec3 forward() const;
+
+    /**
+     * Returns lift-facing vector.
+     * Same as map_to_world(Vec3(0, 1, 0)
+     * @return lift-facing vector
+     */
+    Vec3 left() const;
+
+    /**
+     * Returns up-facing vector.
+     * Same as map_to_world(Vec3(0, 0, 1)
+     * @return up-facing vector
+     */
+    Vec3 up() const;
+
+    /**
+     * Applies the transform to vector vector
+     * @param vector vector to apply transform to
+     * @return transformed vector
+     */
+    Vec3 apply(const Vec3 & vector) const;
+
+    /**
+     * Applies backward transform to vector
+     * @param vector vector to apply backward transform to
+     * @return transformed vector
+     */
+    Vec3 unapply(const Vec3 & vector) const;
 };
 
 #endif //RAY_TRACING_TRANSFORM_H

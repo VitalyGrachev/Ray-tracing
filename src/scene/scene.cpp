ec02b799a7d6dd443c51042c5d29fd51c3ef2d19
Vitaly Grachev <VitalyGrachev@users.noreply.github.com>
2018-09-11 23:15:00 +0700

    Implement Scene's swap methods

--- src/scene/scene.cpp
+++ src/scene/scene.cpp
@@ -1,1 +1,17 @@
 #include "scene.h"
+
+void Scene::swap(Scene & other) {
+    if (this == &other) { return; }
+
+    using std::swap;
+    swap(objects_, other.objects_);
+    swap(light_sources_, other.light_sources_);
+
+    Camera tmp = camera_;
+    camera_ = other.camera_;
+    other.camera_ = tmp;
+}
+
+void swap(Scene & a, Scene & b) {
+    a.swap(b);
+}

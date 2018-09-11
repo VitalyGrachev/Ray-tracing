b26af92f403a0cc23b9f33dbac80da9295a7503d
Vitaly Grachev <VitalyGrachev@users.noreply.github.com>
2018-09-12 00:11:58 +0700

    Implement Scene's find_intersection method

--- src/scene/scene.cpp
+++ src/scene/scene.cpp
@@ -13,6 +13,38 @@ Scene & Scene::operator=(Scene && other) {
     return *this;
 }
 
+std::optional<IntersectionTarget> Scene::find_intersection(const Ray & ray) const {
+    std::optional<IntersectionTarget> closest_target(std::nullopt);
+    float distance_to_closest_target = 0.0f;
+
+    auto closest_intersection_or_nothing = [&](const Geometry & geometry) -> std::optional<Intersection> {
+        auto intersection = geometry.find_intersection(ray);
+        if (intersection.has_value()) {
+            const float distance_to_target = ray.origin().distanceToPoint(intersection->point());
+            if (distance_to_target < distance_to_closest_target ||
+                !closest_target.has_value()) {
+                distance_to_closest_target = distance_to_target;
+                return intersection;
+            }
+        }
+        return {};
+    };
+
+    for (const LightSource & source : light_sources_) {
+        if (auto intersection = closest_intersection_or_nothing(source.geometry())) {
+            closest_target.emplace(*intersection, &source);
+        }
+    }
+
+    for (const Object & object : objects_) {
+        if (auto intersection = closest_intersection_or_nothing(object.geometry())) {
+            closest_target.emplace(*intersection, &object);
+        }
+    }
+
+    return closest_target;
+}
+
 Enumeration<Scene::ObjectsIterator> Scene::enumerate_objects() {
     return Enumeration<Scene::ObjectsIterator>(objects_.begin(), objects_.end());
 }

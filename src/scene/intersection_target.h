982c6e634068b7e9188b975304546d6ca762189c
Vitaly Grachev <VitalyGrachev@users.noreply.github.com>
2018-09-12 23:41:24 +0700

    Encapsulate target obtaining

--- src/scene/intersection_target.h
+++ src/scene/intersection_target.h
@@ -2,22 +2,45 @@
 #define RAY_TRACING_INTERSECTION_TARGET_H
 
 #include <variant>
+#include <optional>
 #include "object/object.h"
 #include "light/light_source.h"
 #include "../geometry/intersection.h"
 
-struct IntersectionTarget {
+class IntersectionTarget {
+public:
     using ObjectTarget = const Object *;
     using LightSourceTarget = const LightSource *;
 
-    Intersection intersection;
-    std::variant<ObjectTarget, LightSourceTarget> target;
-
     IntersectionTarget(const Intersection & intersection, ObjectTarget target)
-            : intersection(intersection), target(target) {}
+            : intersection_(intersection), target_(target) {}
 
     IntersectionTarget(const Intersection & intersection, LightSourceTarget target)
-            : intersection(intersection), target(target) {}
+            : intersection_(intersection), target_(target) {}
+
+    const Intersection & intersection() const { return intersection_; }
+
+    std::optional<ObjectTarget> object_target() const;
+
+    std::optional<LightSourceTarget> light_source_target() const;
+
+private:
+    Intersection intersection_;
+    std::variant<ObjectTarget, LightSourceTarget> target_;
 };
+
+std::optional<IntersectionTarget::ObjectTarget> IntersectionTarget::object_target() const {
+    if (auto target_ptr = std::get_if<ObjectTarget>(&target_)) {
+        return {*target_ptr};
+    }
+    return {};
+}
+
+std::optional<IntersectionTarget::LightSourceTarget> IntersectionTarget::light_source_target() const {
+    if (auto target_ptr = std::get_if<LightSourceTarget>(&target_)) {
+        return {*target_ptr};
+    }
+    return {};
+}
 
 #endif //RAY_TRACING_INTERSECTION_TARGET_H

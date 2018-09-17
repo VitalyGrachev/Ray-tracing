8dc9e1b52a45d9f519c266e6ca5a8734b40c002a
Vitaly Grachev <VitalyGrachev@users.noreply.github.com>
2018-09-17 16:52:12 +0700

    Change LightSource contract. Remove unused files

--- src/scene/light/light.cpp
+++
@@ -1,7 +0,0 @@
-#include "light.h"
-
-#include <algorithm>
-
-Light::Light(const Color & color, float intensity)
-        : color_(color),
-          intensity_(std::clamp(intensity, 0.0f, 1.0f)) {}
--- src/scene/light/light_source.h
+++ src/scene/light/light_source.h
@@ -1,77 +1,21 @@
 #ifndef RAY_TRACING_LIGHT_SOURCE_H
 #define RAY_TRACING_LIGHT_SOURCE_H
 
-#include <memory>
-#include "light.h"
-#include "../../geometry/geometry.h"
-#include "../../util/transform.h"
+#include "illuminance.h"
 
 /**
- * Piece of geometry that emit light
+ * Interface. Represents light source, which can illuminate objects
  */
-class LightSource final {
+class LightSource {
 public:
-    /**
-     * Type of light source
-     */
-    enum class Type {
-        /**
-         * Emits light in only forward direction of this light source.
-         * Brightness is constant for any distance from light source.
-         */
-                Directional,
-
-        /**
-         * Emits light in every direction.
-         * Brightness decreases when distance from light source increases.
-         */
-                Point
-    };
+    virtual ~LightSource() = default;
 
     /**
-     * Creates light source with given properties.
-     * @param light light emitted by the light source
-     * @param source_type type of light source
-     * @param geometry visible geometry of the light source
-     * @param transform transform of the light source
+     * Returns illuminance of given point by the light source
+     * @param point illuminated point
+     * @return illuminance of given point
      */
-    LightSource(const Light & light,
-                Type source_type,
-                const std::shared_ptr<Geometry> & geometry,
-                const Transform & transform = Transform());
-
-    LightSource(const LightSource & other) = default;
-
-    LightSource & operator=(const LightSource & other) = default;
-
-    /**
-     * Returns transform, which represent placement of light source in world coordinate system
-     * @return transform of the light source
-     */
-    Transform & transform() { return transform_; }
-
-    const Transform & transform() const { return transform_; }
-
-    /**
-     * @return Light emitted by light source
-     */
-    const Light & light() const { return light_; }
-
-    /**
-     * @return visible geometry of light source
-     */
-    const Geometry & geometry() const { return *geometry_; }
-
-    /**
-     * @return type of light source
-     */
-    Type source_type() const { return source_type_; }
-
-private:
-    Transform transform_;
-    Light light_;
-    std::shared_ptr<Geometry> geometry_;
-    Type source_type_;
+    virtual Illuminance illuminate_point(const Vec3 & point) const = 0;
 };
 
 #endif //RAY_TRACING_LIGHT_SOURCE_H

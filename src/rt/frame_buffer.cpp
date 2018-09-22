1dab37404611bbdfb8af71fef4b44a35cb267b81
Vitaly Grachev <VitalyGrachev@users.noreply.github.com>
2018-09-22 11:52:15 +0700

    Implement FrameBuffer class

--- src/rt/frame_buffer.cpp
+++ src/rt/frame_buffer.cpp
@@ -1,1 +1,21 @@
 #include "frame_buffer.h"
+
+FrameBuffer::FrameBuffer(const Size & size)
+        : fragment_colors_(size.cols * size.rows, Color(0, 0, 0)),
+          size_(size) {}
+
+FrameBuffer::FrameBuffer(FrameBuffer && other) noexcept
+        : fragment_colors_(std::move(other.fragment_colors_)),
+          size_(other.size_) {
+    other.size_ = {0, 0};
+}
+
+FrameBuffer & FrameBuffer::operator=(FrameBuffer && other) noexcept {
+    if (this != &other) return *this;
+
+    fragment_colors_ = std::move(other.fragment_colors_);
+    size_ = other.size_;
+
+    other.size_ = {0, 0};
+    return *this;
+}
--- src/rt/frame_buffer.h
+++ src/rt/frame_buffer.h
@@ -1,8 +1,48 @@
 #ifndef RAY_TRACING_FRAME_BUFFER_H
 #define RAY_TRACING_FRAME_BUFFER_H
 
-class FrameBuffer {
+#include <vector>
+#include "../util/color.h"
 
+class FrameBuffer {
+public:
+    struct Size {
+        unsigned cols;
+        unsigned rows;
+    };
+
+    struct Position {
+        unsigned col;
+        unsigned row;
+    };
+
+    explicit FrameBuffer(const Size & size);
+
+    FrameBuffer(const FrameBuffer & other) = default;
+
+    FrameBuffer(FrameBuffer && other) noexcept;
+
+    ~FrameBuffer() = default;
+
+    FrameBuffer & operator=(const FrameBuffer & other) = default;
+
+    FrameBuffer & operator=(FrameBuffer && other) noexcept;
+
+    const Size & size() const { return size_; }
+
+    Color & color_at(Position pos) { return fragment_colors_[index(pos)]; }
+
+    const Color & color_at(Position pos) const { return fragment_colors_[index(pos)]; }
+
+private:
+    size_t index(Position pos) const;
+
+    std::vector<Color> fragment_colors_;
+    Size size_;
 };
+
+inline size_t FrameBuffer::index(Position pos) const {
+    return pos.row * size_.cols + pos.col;;
+}
 
 #endif //RAY_TRACING_FRAME_BUFFER_H

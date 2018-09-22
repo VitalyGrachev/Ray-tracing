19790fbb4ae81f4828f09a6196af3ec00160ee54
Vitaly Grachev <VitalyGrachev@users.noreply.github.com>
2018-09-22 11:57:45 +0700

    Create RayTracer inteface

---
+++ src/rt/ray-tracer/ray_tracer.h
@@ -0,0 +1,14 @@
+#ifndef RAY_TRACING_RAY_TRACER_H
+#define RAY_TRACING_RAY_TRACER_H
+
+#include <memory>
+#include "../ray-trace-task/ray_trace_task.h"
+
+class RayTracer {
+public:
+    virtual ~RayTracer() = default;
+
+    virtual void trace_ray(std::unique_ptr<RayTraceTask> && ray) = 0;
+};
+
+#endif //RAY_TRACING_RAY_TRACER_H

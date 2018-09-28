6530b195aab11b0d68e604e5ada993af71456249
Vitaly Grachev <VitalyGrachev@users.noreply.github.com>
2018-09-28 11:19:43 +0700

    Stop using info about Transform internals when building eye rays

--- src/rt/eye_ray_generator.cpp
+++ src/rt/eye_ray_generator.cpp
@@ -24,8 +24,8 @@ Ray EyeRayGenerator::fragment_ray(const Vec2 & angles) const {
     const float cos_y = std::cos(angles.y());
     const float sin_x = std::sin(angles.x());
     const float sin_y = std::sin(angles.y());
-    const Ray ray(Vec3(0, 0, 0), Vec3(cos_y * sin_x, sin_y, cos_y * cos_x));
-    return transform_ray(camera_.transform(), ray);
+    const Transform & tr = camera_.transform();
+    return Ray(tr.origin(), cos_y * cos_x * tr.forward() + cos_y * sin_x * tr.left() + sin_y * tr.up());
 }
 
 Vec2 EyeRayGenerator::fragment_angles(FrameBuffer::Position fragment_pos) const {
@@ -37,9 +37,9 @@ Vec2 EyeRayGenerator::fragment_angles(FrameBuffer::Position fragment_pos) const 
 std::optional<FrameBuffer::Position> EyeRayGenerator::next_fragment_position() {
     const FrameBuffer::Size & size = frame_buffer_.size();
 
-    if(!frame_buffer_.is_position_valid(next_fragment_pos_)) { return std::nullopt; }
+    if (!frame_buffer_.is_position_valid(next_fragment_pos_)) { return std::nullopt; }
 
-    const FrameBuffer::Position fragment_pos  = next_fragment_pos_;
+    const FrameBuffer::Position fragment_pos = next_fragment_pos_;
 
     next_fragment_pos_.row += (next_fragment_pos_.col + 1) / size.cols;
     next_fragment_pos_.col = (next_fragment_pos_.col + 1) % size.cols;

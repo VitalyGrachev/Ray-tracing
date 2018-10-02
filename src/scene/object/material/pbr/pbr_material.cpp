52be275d5eceb639d8785b7949cf773409dba92b
Vitaly Grachev <VitalyGrachev@users.noreply.github.com>
2018-10-02 10:20:38 +0700

    Fix bugs. Light up only faces

--- src/scene/object/material/pbr/pbr_material.cpp
+++ src/scene/object/material/pbr/pbr_material.cpp
@@ -10,20 +10,12 @@ inline float sq(float val) {
     return val * val;
 }
 
-Color cook_torrance_specular(const Vec3 & v, const Vec3 & l, const Vec3 & n,
-                             const Color & base_reflectivity, float roughness) {
-    const Vec3 h = (l + v).normalized();
-    const float norm_coef = 0.25f / (Vec3::dotProduct(n, l) * Vec3::dotProduct(n, v));
-    return norm_coef * GGX_geometric(v, l, n) * GGX_normal_distribution(h, n, roughness) *
-           fresnel_reflectance(base_reflectivity, l, h);
-}
-
 Color lambert_diffuse(const Color & diffuse_color) {
     return diffuse_color / pi;
 }
 
 Color fresnel_reflectance(const Color & base_reflectivity, const Vec3 & l, const Vec3 & h) {
-    return base_reflectivity + (Vec3(1, 1, 1) - base_reflectivity)(1.0f - std::pow(Vec3::dotProduct(l, h), 5.0f));
+    return base_reflectivity + (Color(1, 1, 1) - base_reflectivity) * (1.0f - std::pow(Vec3::dotProduct(l, h), 5.0f));
 }
 
 float GGX_normal_distribution(const Vec3 & h, const Vec3 & n, float roughness) {
@@ -31,14 +23,23 @@ float GGX_normal_distribution(const Vec3 & h, const Vec3 & n, float roughness) {
     return roughness_2 / (pi * sq(sq(Vec3::dotProduct(n, h)) * (roughness_2 - 1) + 1));
 }
 
+float GGX_g(const Vec3 & e, const Vec3 & n, float roughness) {
+    using std::sqrt;
+    const float dot = Vec3::dotProduct(e, n);
+    const float roughness_2 = sq(roughness);
+    return 2.0f * dot / (dot + sqrt(roughness_2 + (1 + roughness_2) * sq(dot)));
+}
+
 float GGX_geometric(const Vec3 & v, const Vec3 & l, const Vec3 & n, float roughness) {
     return GGX_g(v, n, roughness) * GGX_g(l, n, roughness);
 }
 
-float GGX_g(const Vec3 & e, const Vec3 & n, float roughness) {
-    using std::sqrt;
-    const float dot = Vec3::dotProduct(e, n);
-    return 2.0f * dot / (dot + sqrt(sq(roughness) + sq((1 + roughness) * dot)));
+Color cook_torrance_specular(const Vec3 & v, const Vec3 & l, const Vec3 & n,
+                             const Color & base_reflectivity, float roughness) {
+    const Vec3 h = (l + v).normalized();
+    const float norm_coef = 0.25f / (Vec3::dotProduct(n, l) * Vec3::dotProduct(n, v));
+    return norm_coef * GGX_geometric(v, l, n, roughness) * GGX_normal_distribution(h, n, roughness) *
+           fresnel_reflectance(base_reflectivity, l, h);
 }
 
 }
@@ -48,9 +49,13 @@ BRDFunction PBRMaterial::brdf_at(const Vec2 & uv) const {
     const Color diffuse_color = diffuse_color_at(uv);
     const Color base_reflectivity = base_reflectivity_at(uv);
     return BRDFunction([=](const Vec3 & v, const Vec3 & l, const Vec3 & n) {
+        if(Vec3::dotProduct(v, n) <= 0.0f || Vec3::dotProduct(l, n) <= 0.0f) {
+            return Color(0, 0, 0);
+        }
+
         const Color weights = fresnel_reflectance(base_reflectivity, v, n);
         return cook_torrance_specular(v, l, n, base_reflectivity, roughness) * weights +
-               lambert_diffuse(diffuse_color) * (Vec(1, 1, 1) - weights);
+               lambert_diffuse(diffuse_color) * (Color(1, 1, 1) - weights);
     });
 }
 

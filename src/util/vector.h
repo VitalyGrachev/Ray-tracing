61afb01a233a319d3133cfaf452558010081d010
Vitaly Grachev <VitalyGrachev@users.noreply.github.com>
2018-10-02 10:04:19 +0700

    Remove unused Vec4

--- src/util/vector.h
+++ src/util/vector.h
@@ -3,10 +3,18 @@
 
 #include <QVector2D>
 #include <QVector3D>
-#include <QVector4D>
 
 using Vec2 = QVector2D;
 using Vec3 = QVector3D;
-using Vec4 = QVector4D;
+
+inline std::ostream & operator<<(std::ostream & os, const Vec2 & v) {
+    os << '(' << v.x() << ' ' << v.y() << ')';
+    return os;
+}
+
+inline std::ostream & operator<<(std::ostream & os, const Vec3 & v) {
+    os << '(' << v.x() << ' ' << v.y() << ' ' << v.z() << ')';
+    return os;
+}
 
 #endif //RAY_TRACING_VECTOR_H
